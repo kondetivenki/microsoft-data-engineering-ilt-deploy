a112d6eba87a7659790030903383bd7dcff60c28
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2021-04-26 10:45:40 +0530

    Update environment-setup.ps1

--- setup/01/automation/environment-setup.ps1
+++ setup/01/automation/environment-setup.ps1
@@ -20,7 +20,7 @@ if($IsCloudLabs){
         
         Connect-AzAccount -Credential $cred | Out-Null
 
-        $resourceGroupName = (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -like "**DP203-M1**" }).ResourceGroupName
+        $resourceGroupName = (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -like "*DP203-M1*" -and  $_.ResourceGroupName -notlike "*internal*" }).ResourceGroupName
 
         if ($resourceGroupName.Count -gt 1)
         {
