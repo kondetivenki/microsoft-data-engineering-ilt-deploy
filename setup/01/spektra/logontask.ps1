fb4bd3c3d3adf14493acc94aa6f0e12c40bb84ed
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2021-05-05 20:41:11 +0530

    Update logontask.ps1

--- setup/01/spektra/logontask.ps1
+++ setup/01/spektra/logontask.ps1
@@ -3,6 +3,18 @@ Start-Transcript -Path C:\WindowsAzure\Logs\logontasklogs.txt -Append
 #Install power Bi desktop
 Start-Process -FilePath "C:\LabFiles\PBIDesktop_x64.exe" -ArgumentList '-quiet','ACCEPT_EULA=1'
 
+C:\dotnet.exe /silent /install
+
+Write-Information "Checking if Dotnet is installed or not"
+$filetocheck= "C:\dotnet.exe"
+
+if(!(Test-Path $filetocheck -PathType leaf))
+{
+ Write-Information "Powerbi is not installed, Installing it" 
+ C:\dotnet.exe /silent /install
+}
+
+sleep 2
 Cd 'C:\LabFiles\asa\setup\01\automation'
 
 ./environment-setup.ps1
